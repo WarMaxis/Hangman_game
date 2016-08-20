a078c69095f2a477976d1121566caa8e63810cdb
Michał Milanowski <michalmilanowski@op.pl>
2016-08-20 22:45:45 +0200

    Dodanie wirtualnej klawiatury

--- script.js
+++ script.js
@@ -9,18 +9,26 @@ startButton.onclick = function getWord() {
   var length = gameWord.length;
   var hiddenWord = "";
 
-  for (i = 0; i < length; i++) {
-    if (gameWord.charAt(i) == " ") {
-      hiddenWord = hiddenWord + " ";
-    }
-    else if (isNaN(gameWord.charAt(i)) == false) {
-      alert("Hasło nie może zawierać cyfr!\nWpisz nowe hasło.");
-      break;
-    }
-    else {
-      hiddenWord = hiddenWord + "-";
-    }
+  if (gameWord.length == 0) {
+    alert("Wpisz hasło!");
   }
+  else {
+    for (i = 0; i < length; i++) {
+      if (gameWord.charAt(i) == " ") {
+        hiddenWord = hiddenWord + " ";
+      }
+      else if (isNaN(gameWord.charAt(i)) == false) {
+        alert("Hasło nie może zawierać cyfr!\nWpisz nowe hasło.");
+        break;
+      }
+      else {
+        hiddenWord = hiddenWord + "-";
+        document.getElementById('start-game').style.visibility = "hidden";
+        document.getElementById('game-progress').style.visibility = "visible";
+        document.getElementById('game-keyboard').style.visibility = "visible";
+      };
+    };
+  };
 
   document.getElementById('game-word').innerHTML = hiddenWord;
 };
